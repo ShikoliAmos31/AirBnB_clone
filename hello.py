adf311db9046ac635c5ff6f695e9eaf1ce4b90ec
ShikoliAmos31 <amosshikoli@gmail.com>
2024-02-13 11:29:21 -0800

    changes

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
-#!/usr/bin/python3
-def hello(betty):
-    if betty > 98:
-        return True
-    else:
-        return false
+#!/usr/bin/env/python3
+
+class student:
+    def __init__(self, name, greetings):
+        self.name = Betty
+        self.greetings = Hello
+
+    def __str__(self):
+
+        return (
+            f"name: [self.betty]\n greetings: [self.hello]\n"
+            )
